2fed7698206cab83a6c49659760a707116cfa49a
velvelhype <velvelhype@gmail.com>
2022-03-26 02:49:17 +0900

    refact: divide func mlx

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "miniRT.h"
 #include "draw.h"
+#include "parse.h"
 
 int main(int argc, char **argv)
 {
@@ -9,6 +10,7 @@ int main(int argc, char **argv)
 	//	if (argc != 3)
 	//		error_exit();
 	//	parse_rt_file(argv, &rt_info);
+	dummy_parse(&rt_info);
 	//	check_parsed_info(&rt_info);
 	draw(&rt_info);
 }
--- src/parser/dummy_parse.c
+++ src/parser/dummy_parse.c
@@ -1,7 +1,9 @@
 #include "miniRT.h"
-#include "../drawer/draw.h"
+#include "draw.h"
 
 void	dummy_parse(t_rt *rt_info)
 {
 	printf("dummy_parse\n");
+	rt_info->max_height = 400;
+	rt_info->max_width = 400;
 }
