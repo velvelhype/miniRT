5d5187b510509d5b5526acbac7a9fbeae5bd1412
Akihide Ito <39990441+AkkyOrz@users.noreply.github.com>
2022-05-28 20:55:52 +0900

    正規化されていない入力が入ってきた際に正規化する (#24)
    * fix: normarize if orient is not normarized
    
    * fix norm

--- src/parser/parse_light.c
+++ src/parser/parse_light.c
@@ -8,7 +8,7 @@ void	parse_light(t_rt *rt_info, char **tokens, bool is_used)
 		custom_exit("Light: invalid number of arguments");
 	if (tokens[1] == NULL || tokens[2] == NULL || tokens[3] != NULL)
 		custom_exit("Light: invalid number of arguments");
-	rt_info->light.coord = parse_normarized_vector3(tokens[1]);
+	rt_info->light.coord = parse_vector3(tokens[1]);
 	rt_info->light.br_ratio = parse_ratio(tokens[2]);
 	rt_info->light.color = parse_color("255,255,255");
 }
